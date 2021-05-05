257add39d3735b9adabd58eae8c81dfd5518c6c2
ritik723 <ritiksingh7239@gmail.com>
2021-05-05 12:46:43 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import cards from "./Views/Components/Profile/Profile";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Profile from './Views/Components/Profile/Profile'
+import NavbarContainer from './Views/Components/Cards/NavbarContainer'
+import Sidebar from './Views/Components/Sidebar/Sidebar'
 import {
   Card,
   CardText,
@@ -16,6 +19,10 @@ function App() {
   return (
     <div>
       <h2>Template Name</h2>
+      <Profile />
+     <NavbarContainer />
+
+     <Sidebar />
     </div>
   );
 }
